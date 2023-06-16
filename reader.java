72e544d19e6506a8746caf89f26bdfe00480ce0f
ViniTex231 <viniteixeira2828@gmail.com>
2023-06-16 07:46:31 -0300

    encapsulamento

--- reader.java
+++ reader.java
@@ -3,12 +3,17 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class reader {
-    public static void main(String[] args) {
-        String csvFile = "C:/Users/45027278810/Documents/Vini - Python//Integrador_Scrap//output.csv";
-        String line = "";
-        String csvSeparator = ",";
+    private String csvFile;
+    private String csvSeparator;
 
+    public reader(String csvFile, String csvSeparator) {
+        this.csvFile = csvFile;
+        this.csvSeparator = csvSeparator;
+    }
+
+    public void readCSV() {
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(csvSeparator);
                 for (String value : data) {
@@ -19,5 +24,13 @@ public class reader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        String csvFile = "C:/Users/45027278810/Documents/Vini - Python//Integrador_Scrap//output.csv";
+        String csvSeparator = ",";
+
+        reader reader = new reader(csvFile, csvSeparator);
+        reader.readCSV();
     }
 }
